f4d0eff4c670011113ad0ba33472f9db3b3cb76e
Nadeem S <nadeem26707@gmail.com>
2026-03-03 10:24:44 +0530

    Delete UC1WelcomeMessage.java

--- UC1WelcomeMessage.java
+++
@@ -1,7 +0,0 @@
-public class UC1WelcomeMessage {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Book My Stay!");
-        System.out.println("Your one-stop solution for booking hotels and accommodations.");
-        System.out.println("We are delighted to have you here. Let's make your stay memorable!");
-    }
-}
